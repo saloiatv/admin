99e46331a1f60ba7c6e76936b37b0366f4821b5b
joaogsleite <joaogsleite@users.noreply.github.com>
2020-08-24 20:21:14 +0000

    deploy: 03ec9005d45b90e59aa0925221bf294af0c614a4

---
+++ precache-manifest.248df8bef83b3da8d071c3801c552358.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b577970e9a53385620c7960fbcb2702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0740651db3deb04302db",
+    "url": "/static/js/2.bb3c985b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.bb3c985b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f33fe493d7fbc55a65e2",
+    "url": "/static/js/main.14891039.chunk.js"
+  },
+  {
+    "revision": "6a59bd51eb184ae677f3",
+    "url": "/static/js/runtime-main.836b504c.js"
+  }
+]);
